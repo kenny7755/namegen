a2fa78b64a47fe1521bc06f9aefae8f912b6d2aa
Kennedy M <koolkens90@gmail.com>
2022-07-29 03:28:31 -0700

    finished javascript, some new css

--- usergen.js
+++ usergen.js
@@ -1,14 +1,15 @@
-
 const names = ["Kenny","Airfried CPU","Duck","Eternus","test"]
 const btn = document.getElementById("btn");
-
+const nameplace = document.getElementById("nameplace");
 function getRandomUser(){
-    return Math.floor(Math.random() * names[getRandomUser()]);
+    return Math.floor(Math.random() * names.length);
 };
 
 btn.addEventListener('click', function() {
     const randomUser = getRandomUser();
     console.log(randomUser)
+    nameplace.textContent = names[getRandomUser()];
     }); 
 
-    const name = document.querySelector("nameplace").innerHTML = randomUser;
+document.getElementById("nameplace").innerHTML = names[getRandomUser()];
+
